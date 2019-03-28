0a853da1798f663c1414149d0376f09b5ca0f9fb
Default user <l.larrota10@uniandes.edu.co>
2019-03-28 12:51:13 +0000

    primer intento

---
+++ complejo.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Complejo():
+    def __init__(self, a, b):
+        self.imaginario=b
+        self.real=a
+        self.norma=np.sqrt((a**2)+(b**2))
+        
+    def conjugado(a,b):
+        a, -self.imaginario
+        
+    def calcula_norma(a,b):
+        self.norma
+    
+    def pow(n):
+        self.real
+        
